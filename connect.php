19fed7b5b64384a4661ffb0d1468b3b5d63374af
sultanhaniff <37823513+sultanhaniff@users.noreply.github.com>
2018-07-16 11:00:42 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$host='localhost';
+$user='root';
+$pass='';
+$database='kata_dasar';
+
+$conn=mysql_connect($host,$user,$pass);
+mysql_select_db($database);
+
+?>
---
+++ upload.php
@@ -0,0 +1,7 @@
+<html>
+<title>Form Upload</title>
+<body>
+<form enctype="multipart/form-data" method="POST" action="hasil_upload.php">
+File yang di upload : <input type="file" name="fupload"><br>
+<input type=submit value=Upload>
+</form>
